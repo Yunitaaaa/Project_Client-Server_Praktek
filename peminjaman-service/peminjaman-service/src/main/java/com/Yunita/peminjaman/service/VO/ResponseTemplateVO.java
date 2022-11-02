ef92eed5bd94954c84c7915611f44cacf6f88a4f
Yunitaaaa <113493671+Yunitaaaa@users.noreply.github.com>
2022-11-02 00:23:49 -0700

    Add files via upload
    jdk-17.0.4.1
    Spring Boot 2.7.5
    Packaging Jar

---
+++ peminjaman-service/peminjaman-service/src/main/java/com/Yunita/peminjaman/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Yunita.peminjaman.service.VO;
+
+import com.Yunita.peminjaman.service.entity.Peminjaman;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author acer
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+
+    public void setPeminjaman(Peminjaman peminjaman) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setAnggota(Anggota anggota) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ peminjaman-service/peminjaman-service/src/main/java/com/Yunita/peminjaman/service/repository/PeminjamanRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Yunita.peminjaman.service.repository;
+
+import com.Yunita.peminjaman.service.entity.Peminjaman;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author acer
+ */
+@Repository
+public interface PeminjamanRepository extends JpaRepository<Peminjaman, Long> {
+
+    public Peminjaman findByPeminjamanId(Long peminjamanId);
+
+}
