b548e161d4d263e4f44ea969ff5d37e71db89a67
Yunitaaaa <113493671+Yunitaaaa@users.noreply.github.com>
2022-11-05 16:26:13 -0700

    Add files via upload
    jdk-17.0.4.1
    Spring Boot 2.7.5
    Packaging Jar

---
+++ pengembalian-service/src/main/java/com/Yunita/pengembalian/service/VO/ResponseTemplateVo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Yunita.pengembalian.service.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author acer
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVo {
+
+    private Peminjaman peminjaman;
+    private Pengembalian pengembalian;
+
+}
---
+++ pengembalian-service/src/main/java/com/Yunita/pengembalian/service/service/PengembalianService.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Yunita.pengembalian.service.service;
+
+import com.Yunita.pengembalian.service.VO.Peminjaman;
+import com.Yunita.pengembalian.service.VO.ResponseTemplateVo;
+import com.Yunita.pengembalian.service.VO.peminjaman;
+import com.Yunita.pengembalian.service.entity.Pengembalian;
+import com.Yunita.pengembalian.service.repository.PengembalianRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author acer
+ */
+@Service
+public class PengembalianService {
+
+    @Autowired
+    private PengembalianRepository pengembalianRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Pengembalian savePengembalian(Pengembalian pengembalian) {
+        return pengembalianRepository.save(pengembalian);
+    }
+
+    public ResponseTemplateVo getPengembalian(Long pengembalianId) {
+        ResponseTemplateVo vo = new ResponseTemplateVo();
+        Pengembalian pengembalian = 
+                pengembalianRepository.findByPengembalianId(pengembalianId);
+
+        Peminjaman peminjaman = 
+                restTemplate.getForObject("http://localhost:9003/pengembalian/" + pengembalian.getPeminjamanId(), Peminjaman.class);
+
+        vo.setPeminjaman(peminjaman);
+        vo.setPengembalian(pengembalian);
+        return vo;
+    }
+
+}
