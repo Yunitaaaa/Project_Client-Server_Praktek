40ea553054fc68a4afd1bf98297d3f4227317bcd
Yunitaaaa <113493671+Yunitaaaa@users.noreply.github.com>
2022-12-13 00:05:05 -0800

    Add files via upload
    jdk-17.0.4.1

---
+++ Client_2101082022/Client_2101082022/src/main/java/com/yunita/client_2101082022/controller/AnggotaController.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yunita.client_2101082022.controller;
+
+import com.yunita.client_2101082022.Form_Anggota;
+import com.yunita.client_2101082022.model.Anggota;
+import com.yunita.client_2101082022.service.AnggotaService;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Acer
+ */
+public class AnggotaController {
+    private AnggotaService anggotaService;
+    private Form_Anggota formAnggota;
+    
+    public AnggotaController(Form_Anggota formAnggota){
+        this.formAnggota = formAnggota;
+        anggotaService = new AnggotaService();
+    }
+    
+    public void bersihForm(){
+        formAnggota.getTxtIdAnggota().setText("");
+        formAnggota.getTxtNama().setText("");
+        formAnggota.getTxtAlamat().setText("");
+    }
+    
+    public Anggota saveAnggota(){
+        Anggota anggota = new Anggota();
+        anggota.setNama(formAnggota.getTxtNama().getText());
+        anggota.setAlamat(formAnggota.getTxtAlamat().getText());
+        return anggotaService.saveAnggota(anggota);
+    }
+    
+    public Anggota getAnggotaId(){
+        Long id = Long.parseLong(formAnggota.getTxtIdAnggota().getText());
+        Anggota anggota = anggotaService.getAnggota(id);
+        if(anggota!=null){
+            formAnggota.getTxtNama().setText(anggota.getNama());
+            formAnggota.getTxtAlamat().setText(anggota.getAlamat());
+        }else{
+            JOptionPane.showMessageDialog(formAnggota, "Data tidak ada");
+        }
+        return anggota;
+    }
+}
