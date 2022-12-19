8558106d19bb0e6b1aee4b78b4ea6336a1871511
Yunitaaaa <113493671+Yunitaaaa@users.noreply.github.com>
2022-12-19 06:49:56 -0800

    Add files via upload
    jdk-17.0.4.1
    Pakaging jar

---
+++ peminjaman-service/src/main/java/com/Yunita/peminjaman/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Yunita.peminjaman.service.VO;
+
+import com.Yunita.peminjaman.service.entity.Peminjaman;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author acer
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+    private Buku buku;
+
+    public void setBuku(Buku buku) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setAnggota(Anggota anggota) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setPeminjaman(Peminjaman peminjaman) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ peminjaman-service/src/main/java/com/Yunita/peminjaman/service/service/PeminjamanService.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Yunita.peminjaman.service.service;
+
+import com.Yunita.peminjaman.service.VO.Anggota;
+import com.Yunita.peminjaman.service.VO.ResponseTemplateVO;
+import com.Yunita.peminjaman.service.entity.Peminjaman;
+import com.Yunita.peminjaman.service.repository.PeminjamanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author acer
+ */
+@Service
+public class PeminjamanService {
+    @Autowired
+    private PeminjamanRepository peminjamanRepository;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    public Peminjaman savePeminjaman(Peminjaman peminjaman){
+        return peminjamanRepository.save(peminjaman);
+    }
+    
+    public ResponseTemplateVO getPeminjaman(Long peminjamanId){
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        Peminjaman peminjaman = 
+                peminjamanRepository.findByPeminjamanId(peminjamanId);
+        Anggota anggota = 
+        restTemplate.getForObject("http://localhost:9001/anggota/"
+                + peminjaman.getAnggotaId(), Anggota.class);
+        vo.setPeminjaman(peminjaman);
+        vo.setAnggota(anggota); 
+        return vo;
+    }
+    
+}
+
