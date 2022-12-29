81d43320c9165909cb46b88d3345d0a0875b2630
Yunitaaaa <113493671+Yunitaaaa@users.noreply.github.com>
2022-12-29 08:10:32 -0800

    Add files via upload
    jdk-17.0.4.1
    Spring Boot 3.0.0
    Packaging Jar

---
+++ gaji-service-main/src/main/java/com/yunita/gaji/service/vo/ResponseTemplateVO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yunita.gaji.service.vo;
+
+import com.yunita.gaji.service.entity.Gaji;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Acer
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+
+    private Gaji gaji;
+    private Karyawan karyawan;
+    private Golongan golongan;
+
+}
